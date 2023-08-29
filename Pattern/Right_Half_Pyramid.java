1975a5591109541cc3f98b25e8ef7f9eae2210d2
Prince Mishra <princemishraa588@gmail.com>
2023-08-29 19:37:17 +0530

    Create Right_Half_Pyramid.java

---
+++ Pattern/Right_Half_Pyramid.java
@@ -0,0 +1,25 @@
+// Right Half Pyramid pattern in Java
+/*
+OUTPUT :-
+*
+* *
+* * *
+* * * *
+* * * * *
+
+*/
+public class Right_Half_Pyramid 
+{
+  public static void main(String[] args) 
+  {
+    int rows = 5;
+    for (int i = 1; i <= rows; i++) 
+    {
+      for (int j = 1; j <= i; j++) 
+      {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
